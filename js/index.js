377370423bd5e37eef01f015e8e1c524bd466a4e
codeL0gic <shannon.neumann@yum.com>
2025-01-14 19:46:01 -0500

    main

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 // Prevent us from attempting to use variables
 // that are not declared
-"use strict"
+"use strict";
+/*
+let healthExercise = ["running", "walking", "yoga", "biking"];
+console.log(healthExercise);
+
+console.log(healthExercise[0]);
+
+healthExercise = ["running", "walking", "yoga", "biking"];
+healthExercise.push("swimming");
+console.log(healthExercise);
+
+healthExercise.shift();
+console.log(healthExercise);
+
+let getCars = ["Toyota", "Honda", "Ford", "Chevy"];
+console.log(getCars);
+getCars.push("BMW");
+console.log(getCars);
+getCars.shift();
+console.log(getCars);
+*/
