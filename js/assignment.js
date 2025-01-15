c6250341b586fba599eb6eafc8cdfb0a1f5ec47e
codeL0gic <shannon.neumann@yum.com>
2025-01-15 17:48:08 -0500

    main

--- js/assignment.js
+++ js/assignment.js
@@ -1,15 +1,16 @@
 "use strict";
 
 const todos = [
-    "Wash the dishes",
-    "Clean the gutters",
-    "Mow the yard",
-    "Do some coding",
-    "Rake the leaves",
+  "Wash the dishes",
+  "Clean the gutters",
+  "Mow the yard",
+  "Do some coding",
+  "Rake the leaves",
 ];
 
 // Declare your two immutable variables
-
+let shiftValue;
+let popValue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,46 +23,54 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+  answerOneEl.innerHTML = todos;
 }
 
 function updateAnswerTwo() {
-
+  answerTwoEl.innerHTML = todos[2];
 }
 
 function removeLastValue() {
-
+  popValue = todos.pop();
 }
 
 function removeFirstValue() {
-
+  shiftValue = todos.shift();
 }
 
 function addShiftAndPopValues() {
-
+  todos.push(shiftValue, popValue);
 }
 
 function updateAnswerFour() {
-
+  answerFourEl.innerHTML = todos;
 }
 
 function reverseTodoList() {
-
+  todos.reverse();
 }
 
 function updateAnswerFive() {
-
+  answerFiveEl.innerHTML = todos;
 }
 
 function updateAnswerSix() {
-
+  answerSixEl.innerHTML = todos;
 }
 
 function render() {
-
+  updateAnswerOne();
+  updateAnswerTwo();
+  removeLastValue();
+  updateAnswerFour();
+  reverseTodoList();
+  updateAnswerFive();
+  removeFirstValue();
+  addShiftAndPopValues();
+  updateAnswerSix();
 }
 
 submissionBtn.addEventListener("click", function () {
-    // Update this function
-    render();
+  // Update this function
+  render();
 });
